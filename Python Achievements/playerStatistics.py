7016bd3e6c1c1fcb6787fb26f296bbe3bc98c573
Jayden <macbeanjayden@gmail.com>
2020-09-16 13:54:13 +0200

    Statistics

---
+++ Python Achievements/playerStatistics.py
@@ -0,0 +1,24 @@
+naam = "Mr.Krabs"
+Strength = 15
+Magic = "Absolute shit"
+Skill = "Godlike"
+Luck = "Gettin' money out of nowhere"
+Speed = "He is speed"
+Defense = 99
+Resistance = 0
+Support = False
+
+
+print("Unit: " + naam)
+print("Strength: " + str(Strength))
+print("Magic: " + Magic)
+print("Skill: " + Skill)
+print("Luck: " + Luck)
+print("Speed: " + Speed)
+print("Defense: " + str(Defense))
+print("Resistance: " + str(Resistance))
+if(Support == True):
+    print("Support Level: C for suCky")
+elif(Support == False):
+    print("Support Level: A for AmAzing")
+
