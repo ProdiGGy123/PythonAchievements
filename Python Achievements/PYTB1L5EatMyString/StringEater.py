e4d1ac5ed427b78d0494ddbcb8e1b0b7cba78fab
Jayden <macbeanjayden@gmail.com>
2020-09-30 15:28:29 +0200

    Python Les 4 Where's Waldo

---
+++ Python Achievements/PYTB1L5EatMyString/StringEater.py
@@ -0,0 +1,8 @@
+def FunctionStringEater():
+    s = "Hallo ik ben een string en ik wordt opgegeten"
+    ss = s[0:len(s)-1]
+    print(ss)
+
+FunctionStringEater()
+
+
---
+++ Python Achievements/PYTB1L5EatMyString/TestString.py
@@ -0,0 +1,2 @@
+s = "Hallo ik ben een string en ik wordt opgegeten"
+print(len(s))
