8e629d0406bbea820a2a9da96cd9a099f0363290
Jayden <macbeanjayden@gmail.com>
2020-10-07 14:25:01 +0200

    Les 6 :D

---
+++ Python Achievements/PYTB1L6CanYouHandleTheList/ZaListo.py
@@ -0,0 +1,8 @@
+ZaListo = ["369", 369, float(9.6), "I am the EGGMAN", 0]
+ZaListo.reverse()
+print(ZaListo)
+print(ZaListo.copy())
+ZaListo.remove("369")
+print(ZaListo)
+ZaListo.insert(2, "That's all I am")
+print(ZaListo)
